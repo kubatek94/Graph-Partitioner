9662fb4b6c07c424029f339e0d1b403e5376a99d
Jakub Gawron <kubatek94@gmail.com>
2016-04-29 13:43:39 +0100

    Updated SortedArray to be more generic and provided description.

--- src/main/java/uk/co/kubatek94/util/SortedArray.java
+++ src/main/java/uk/co/kubatek94/util/SortedArray.java
@@ -1,7 +1,5 @@
 package uk.co.kubatek94.util;
 
-import uk.co.kubatek94.graph.V;
-
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Comparator;
@@ -9,17 +7,24 @@ import java.util.Iterator;
 
 /**
  * Created by kubatek94 on 29/04/16.
+ * SortedArray provides good performance for data which gets added once and then only gets reduced.
+ *
+ * It uses comparator to sort the elements first, so that it can use binary search later to find the elements quickly.
+ * Important: Comparator must return a single exact match, so that each element can be identified uniquely.
+ * Otherwise binary search won't work, as it does not provide guarantee to return first matching element if they are equal in the comparator.
+ *
+ * It provides methods to remove elements from the beginning and the end of the array (removeFirst, removeLast).
+ * It does not resize the array. It does not provide boundary checks.
  */
 public class SortedArray<T> {
-    private T vertices[];
+    private T elements[];
+    private Comparator<T> comparator;
 
     private int leftIndex = 0;
     private int rightIndex = 0;
 
-    private Comparator<T> comparator;
-
     public SortedArray(int size, Comparator<T> comparator) {
-        this.vertices = (T[]) new Object[size];
+        this.elements = (T[]) new Object[size];
         this.comparator = comparator;
     }
 
@@ -31,14 +36,14 @@ public class SortedArray<T> {
     public void addAll(Collection<T> collection) {
         Iterator<T> iterator = collection.iterator();
         while (iterator.hasNext()) {
-            vertices[rightIndex] = iterator.next();
+            elements[rightIndex] = iterator.next();
             rightIndex++;
         }
-        Arrays.sort(vertices, comparator);
+        Arrays.sort(elements, comparator);
     }
 
     public T get(int index) {
-        return vertices[leftIndex + index];
+        return elements[leftIndex + index];
     }
 
     public int size() {
@@ -46,13 +51,13 @@ public class SortedArray<T> {
     }
 
     public T removeFirst() {
-        T vertex = vertices[leftIndex++];
-        return vertex;
+        T element = elements[leftIndex++];
+        return element;
     }
 
     public T removeLast() {
-        T vertex = vertices[--rightIndex];
-        return vertex;
+        T element = elements[--rightIndex];
+        return element;
     }
 
     public T remove(int index) {
@@ -62,7 +67,7 @@ public class SortedArray<T> {
             System.out.format("Found index %d\n", index);
             return null;
         } else {
-            T element = vertices[index];
+            T element = elements[index];
 
             //find index of the middle of the data
             int middle = ((leftIndex+rightIndex) / 2);
@@ -70,13 +75,13 @@ public class SortedArray<T> {
             //if our element is closer to the right side of the array
             if (index > middle) {
                 //shift elements past the index to the left
-                System.arraycopy(vertices, index + 1, vertices, index, rightIndex - index - 1);
+                System.arraycopy(elements, index + 1, elements, index, rightIndex - index - 1);
                 rightIndex--;
 
                 //else our element is closer to the left side of the array
             } else {
                 //shift elements before the index to the right
-                System.arraycopy(vertices, leftIndex, vertices, leftIndex + 1, index - leftIndex);
+                System.arraycopy(elements, leftIndex, elements, leftIndex + 1, index - leftIndex);
                 leftIndex++;
             }
 
@@ -84,12 +89,12 @@ public class SortedArray<T> {
         }
     }
 
-    public void remove(T vertex) {
-        int index = findIndex(vertex);
+    public void remove(T element) {
+        int index = findIndex(element);
         remove(index);
     }
 
-    public int findIndex(T vertex) {
-        return Arrays.binarySearch(vertices, leftIndex, rightIndex, vertex, comparator) - leftIndex;
+    public int findIndex(T element) {
+        return Arrays.binarySearch(elements, leftIndex, rightIndex, element, comparator) - leftIndex;
     }
 }
