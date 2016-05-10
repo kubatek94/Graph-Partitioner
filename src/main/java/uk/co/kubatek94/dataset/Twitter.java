1e54ed8a47fa3f7d3e84eebc66866127d8df28b1
Jakub Gawron <kubatek94@gmail.com>
2016-05-10 18:11:51 +0100

    Added twitter dataset

--- src/main/java/uk/co/kubatek94/dataset/Twitter.java
+++ src/main/java/uk/co/kubatek94/dataset/Twitter.java
@@ -1,7 +1,24 @@
 package uk.co.kubatek94.dataset;
 
+import uk.co.kubatek94.graph.E;
+
+import java.util.stream.Stream;
+
 /**
  * Created by kubatek94 on 10/05/16.
  */
-public class Twitter {
+public class Twitter extends Dataset {
+    private final static String path = "/home/kubatek94/Desktop/DissertationProject/datasets/twitter/twitter_combined.csv";
+
+    public Twitter() {
+        super(path);
+    }
+
+    @Override
+    public Stream<E> getEdgeStream() {
+        return stream
+                .parallel()
+                .map(s -> s.split(" ")) //split the line, so that it becomes an edge between two node ids
+                .map(e -> new E(e[0], e[1]));
+    }
 }
